325a95accc025f5f251e2e88e26fcdc2aead18cd
Micah Bowie <micahbowie20@gmail.com>
2020-09-07 18:42:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     time = Time.new
  
-    if time.strftime("%k") <= 11
+    if time.strftime("%k") <= "11"
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon"
